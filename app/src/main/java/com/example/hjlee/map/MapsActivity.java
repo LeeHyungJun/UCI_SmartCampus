5d2b3ba21049f1942a79c8a571a1467f2834c901
H.J.Lee <hyungjoon_@naver.com>
2015-07-23 01:31:53 -0700

    all picture update complete

--- app/src/main/java/com/example/hjlee/map/MapsActivity.java
+++ app/src/main/java/com/example/hjlee/map/MapsActivity.java
@@ -20,16 +20,16 @@ public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
-    static final LatLng artGallery = new LatLng(33.649714, -117.844649);
-    static final LatLng javaCity = new LatLng(33.643434, -117.841239);
-    static final LatLng busStop = new LatLng(33.649427, -117.839849);
-    static final LatLng foodCoat = new LatLng(33.648788, -117.842271);
-    static final LatLng starBucks = new LatLng(33.648260, -117.842082);
-    static final LatLng socialPlaza = new LatLng(33.646751, -117.839257);
-    static final LatLng aldrichPark = new LatLng(33.646462, -117.843710);
-    static final LatLng FredrickReinesHall = new LatLng(33.643860, -117.843573);
-    static final LatLng AIRC_Terrace = new LatLng(33.646161, -117.840576);
-
+    static final LatLng universityartgallery = new LatLng(33.649714, -117.844649);
+    static final LatLng javacity = new LatLng(33.643434, -117.841239);
+    static final LatLng busstop = new LatLng(33.649427, -117.839849);
+    static final LatLng foodcourt = new LatLng(33.648788, -117.842271);
+    static final LatLng starbucks = new LatLng(33.648260, -117.842082);
+    static final LatLng musicmediabldg = new LatLng(33.649269, -117.844609);
+    static final LatLng aldrichpark = new LatLng(33.646012, -117.842749);
+    static final LatLng frederickreineshall = new LatLng(33.643860, -117.843573);
+    static final LatLng aircterrace = new LatLng(33.646161, -117.840576);
+    static final LatLng library = new LatLng(33.647261, -117.841328);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +37,16 @@ public class MapsActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
         setUpMapIfNeeded();
 
-        Marker marker = mMap.addMarker(new MarkerOptions().position(artGallery).title("University Art Gallery").alpha(0.7f));
-        Marker b = mMap.addMarker(new MarkerOptions().position(javaCity).title("Java City").alpha(0.7f));
-        Marker c = mMap.addMarker(new MarkerOptions().position(busStop).title("Bus Stop").alpha(0.7f));
-        Marker d = mMap.addMarker(new MarkerOptions().position(foodCoat).title("Food Court").alpha(0.7f));
-        Marker e = mMap.addMarker(new MarkerOptions().position(starBucks).title("Star Bucks").alpha(0.7f));
-        Marker f = mMap.addMarker(new MarkerOptions().position(socialPlaza).title("socialPlaza").alpha(0.7f));
-        Marker g = mMap.addMarker(new MarkerOptions().position(aldrichPark).title("Aldrich Park").alpha(0.7f));
-        Marker h = mMap.addMarker(new MarkerOptions().position(FredrickReinesHall).title("Fredrick Reines Hall").alpha(0.7f));
-        Marker i = mMap.addMarker(new MarkerOptions().position(AIRC_Terrace).title("AIRC Terrace").alpha(0.7f));
-
-
+        Marker a = mMap.addMarker(new MarkerOptions().position(universityartgallery).title("University Art Gallery").alpha(0.7f));
+        Marker b = mMap.addMarker(new MarkerOptions().position(javacity).title("Java City").alpha(0.7f));
+        Marker c = mMap.addMarker(new MarkerOptions().position(busstop).alpha(0.7f));
+        Marker d = mMap.addMarker(new MarkerOptions().position(foodcourt).title("Food Court").alpha(0.7f));
+        Marker e = mMap.addMarker(new MarkerOptions().position(starbucks).title("Star Bucks").alpha(0.7f));
+        Marker f = mMap.addMarker(new MarkerOptions().position(musicmediabldg).title("Music Medical Bldg").alpha(0.7f));
+        Marker g = mMap.addMarker(new MarkerOptions().position(aldrichpark).title("Aldrich Park").alpha(0.7f));
+        Marker h = mMap.addMarker(new MarkerOptions().position(frederickreineshall).title("Frederick Reines Hall").alpha(0.7f));
+        Marker i = mMap.addMarker(new MarkerOptions().position(aircterrace).title("AIRC Terrace").alpha(0.7f));
+        Marker j = mMap.addMarker(new MarkerOptions().position(library).title("Library").alpha(0.7f));
 
         mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
             public void onInfoWindowClick(Marker arg0) {
@@ -55,28 +54,32 @@ public class MapsActivity extends FragmentActivity {
 
                 LinearLayout bottom_layout = (LinearLayout) findViewById(R.id.bottomLayout);
                 TextView myText = (TextView)findViewById(R.id.bottomText);
+                myText.setText(arg0.getTitle());
+                ImageView image = (ImageView) findViewById(R.id.slidingImage);
 
-
-                //myText.setVisibility(View.VISIBLE);
-
-                //layout.setVisibility(View.VISIBLE);
-                //myText.setVisibility(View.VISIBLE);
-
-                //ImageView layout2 = (ImageView) findViewById(R.id.image);
-
-
-               // bottom_layout.setVisibility(View.VISIBLE);
-
-                    myText.setText(arg0.getTitle());
-
-                   // layout1.setVisibility(View.VISIBLE);
-                   // layout2.setVisibility(View.VISIBLE);
+                if(arg0.getPosition().equals(universityartgallery))
+                   image.setImageResource(R.drawable.universityartgallery);
+                else if(arg0.getPosition().equals(javacity))
+                    image.setImageResource(R.drawable.javacity);
+                else if(arg0.getPosition().equals(busstop))
+                    image.setImageResource(R.drawable.busstop);
+                else if(arg0.getPosition().equals(foodcourt))
+                    image.setImageResource(R.drawable.foodcourt);
+                else if(arg0.getPosition().equals(starbucks))
+                    image.setImageResource(R.drawable.starbucks);
+                else if(arg0.getPosition().equals(musicmediabldg))
+                    image.setImageResource(R.drawable.musicmediabldg);
+                else if(arg0.getPosition().equals(aldrichpark))
+                    image.setImageResource(R.drawable.aldrichpark);
+                else if(arg0.getPosition().equals(frederickreineshall))
+                    image.setImageResource(R.drawable.frederickreineshall);
+                else if(arg0.getPosition().equals(aircterrace))
+                    image.setImageResource(R.drawable.aircterrace);
+                else if(arg0.getPosition().equals(library))
+                    image.setImageResource(R.drawable.library);
 
             }
         });
-
-
-
 
     }
 
